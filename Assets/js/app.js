9741f8a622cdc6331ce59111cb13e6f38aa21330
jonathan gauvin <dadou97480@hotmail.frgit>
2021-09-19 11:59:12 +0400

    First Submit
    first submission of the One page for the VBP DEPOT SERVICES site

---
+++ Assets/js/app.js
@@ -0,0 +1,25 @@
+/* Config Scroll Reveal */
+ScrollReveal().reveal('.presentation,.block,.contact,.organigramme,.depotage', { delay: 250, duration: 1000 });
+
+ScrollReveal().reveal('.info-block,.if1,.if2,.if3,', { delay: 500, duration: 1000 });
+
+ScrollReveal().reveal('.c1', { delay: 500, duration: 1000 });
+
+ScrollReveal().reveal('.c2', { delay: 750, duration: 1000 });
+
+ScrollReveal().reveal('.c3', { delay: 1000, duration: 1000 });
+
+
+/**
+ * 
+ * @param {HTMLElement} element 
+ */
+function ToggleResponceMenu() {
+    document.getElementById('res-block').classList.toggle('mobile-menu-active');
+}
+
+
+
+
+
+
